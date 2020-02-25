5ba22d2ad7f8021859e58287132a119689249bd2
Ivan Nikitin <iamvano@mail.ru>
2020-02-25 06:15:35 +0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+// The project version
+const version = "1.0.0"
+
+// The max count of the goroutines which work simultaneously
+const maxConcurrency = 5
+
+// Look for the following word
+const target = "Go"
+
+type semaphoreInterface interface {
+	Acquire()
+	Release()
+}
+
+type semaphoreImpl struct {
+	sem chan struct{}
+}
+
+func (s *semaphoreImpl) Acquire() {
+	s.sem <- struct{}{}
+}
+
+func (s *semaphoreImpl) Release() {
+	_ = <-s.sem
+}
+
+func semaphore(tickets int) semaphoreInterface {
+	return &semaphoreImpl{
+		sem: make(chan struct{}, tickets),
+	}
+}
+
+type resultWriter = func(string, int)
+
+func urlHandler(url string, resWriter resultWriter) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "urlHandler:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "urlHandler:", err)
+		return
+	}
+
+	resWriter(url, strings.Count(string(body), target))
+}
+
+func stdInProcessing(resWriter resultWriter) {
+
+	wg := new(sync.WaitGroup)
+	s := semaphore(maxConcurrency)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		url := scanner.Text()
+		if url == "exit" {
+			break
+		}
+
+		s.Acquire()
+
+		wg.Add(1)
+		go func() {
+			defer func() {
+				wg.Done()
+				s.Release()
+			}()
+			urlHandler(url, resWriter)
+		}()
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	fmt.Printf("FinalTask v%s, Feb 2020\n", version)
+	fmt.Println("Please, input the URL, URL list or 'exit'")
+
+	mutex := new(sync.Mutex)
+	var total int64
+
+	resWriter := func(url string, n int) {
+		mutex.Lock()
+		defer mutex.Unlock()
+		fmt.Printf("Count for %s: %d\n", url, n)
+		total += int64(n)
+	}
+
+	stdInProcessing(resWriter)
+
+	fmt.Println("Total:", total)
+	fmt.Println("Successful complete")
+}
